d612bd08623b7ab0b0780ab75bc608b125cf2b07
Asim-Raju-Sayyad <asimsayyad646@gmail.com>
2025-08-01 00:21:15 +0530

    Added routes for users and books

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const {users} = require("./data/users.json")
+
+const userRouter = require("./routes/users.js");
+const booksRouter = require("./routes/books");
 
 const app = express();
 
@@ -7,20 +9,18 @@ const PORT = 8081;
 
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.status(200).json({
-        message: "Server is up and running.....",
-    })
-})
-
-app.get("/users", (req, res) => {
+// http://localhost:8081/users/
+http: app.get("/", (req, res) => {
   res.status(200).json({
-    success: true,
-    data: users,
+    message: "Server is up and running :-)",
+    data: "hey",
   });
 });
 
+app.use("/users", userRouter);
+app.use("/books", booksRouter);
 
-app.listen(PORT, ()=>{
-    console.log(`Server is runing at port ${PORT}`)
-})
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
